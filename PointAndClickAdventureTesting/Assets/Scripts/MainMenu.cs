f184d63589491bd634b7696536035ab90f669a3a
Al <theonlypurplecat@gmail.com>
2021-06-28 11:54:56 +1200

    unity project and basic menu setting up

---
+++ PointAndClickAdventureTesting/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject mainMenuPage;
+
+    [SerializeField]
+    private GameObject quitPromptPage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+
+
+}
