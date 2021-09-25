d0d145f856649d400ce27ea570eddd594555bff7
Alex <theonlypurplecat@gmail.com>
2021-09-25 16:17:33 +1200

    halfway thoguht making dialogue

---
+++ PointAndClickAdventureTesting/Assets/Scripts/Dialouge/DialougeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialouge dialouge)
+    {
+        Debug.Log("Starting Conversation with Kara");
+    }
+
+}
